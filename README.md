c2f9de420f64eb148a4cd5427dffdf932b5cae5a
Sam Wilson <sam@samwilson.id.au>
2013-02-14 13:25:11 +0800

    General clean-up and tweaks:
      * Change to jQuery
      * Prevent form submission when no title has been entered
      * Code formatting
      * Allow more than one NEWPAGE form on a page
      * Declare as a 'block' type of output

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-dokuwiki-plugin-addnewpage
-==========================
+Dokuwiki Plugin: Add New Page
+=============================
 
-The add new page plugin from https://www.dokuwiki.org/plugin:addnewpage
+The add new page plugin for Dokuwiki: https://www.dokuwiki.org/plugin:addnewpage
+
+# Syntax
+
+    {{NEWPAGE[>namespace]}}
+
+Where namespace is optional, and indicates the destination namespace for the new page.
+
+# Authors
+
+- Originally written by iDo, 2006.  Assumed to be released under GPL 2.
+- Moved to Github by hamstar <hamstar@telescum.co.nz>, 2012.
+- Updated by Sam Wilson <sam@samwilson.id.au>, 2013.
--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-/*USE : UTF8*/
-function setName() {
-	document.getElementById("editform").setAttribute("action","?id="+document.getElementById('np_cat').value+':'+document.getElementById('addnewpage_title').value);
-}
+
+jQuery(document).ready(function() {
+
+    // Start with disabled submit button
+    jQuery(".addnewpage :submit").attr("disabled", "disabled");
+    // Then enable it when a title is entered
+    jQuery(".addnewpage input[name='title']").keyup(function(){
+        var $submit = jQuery(this).parent("form").find(":submit");
+        if (jQuery(this).val().length > 0) {
+            $submit.removeAttr("disabled");
+        } else {
+            // For when the user deletes the text
+            $submit.attr("disabled", "disabled");
+        }
+    });
+
+    // Change the form's action on submit
+    $editform = jQuery(".addnewpage form").submit(function(e) {
+        var ns = jQuery(this).find("[name='np_cat']").val();
+        var title = jQuery(this).find("input[name='title']").val();
+        var action = DOKU_BASE+"?do=edit&id="+ns+":"+title;
+        jQuery(this).attr("action", action);
+        return true;
+    });
+
+});
