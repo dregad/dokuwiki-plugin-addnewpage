b2d702e67b4c864358ee29ca90bfc4f3f2e5148a
lawzovqoye <umpcvwgdfv@rzoeqr.com>
2014-02-26 09:25:05 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,8 +1,10 @@
 <?php
-/*
- * English language file
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author lawzovqoye <umpcvwgdfv@rzoeqr.com>
  */
-$lang['namespaceRoot'] = "Root";
-$lang['okbutton']      = "Add page";
-$lang['nooption']      = "You are not allowed to add pages";
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespaceRoot']         = 'ewjktusbotmbuf, <a href="http://www.oeljkjgzrq.com/">akmfajedvv</a> ';
+$lang['okbutton']              = 'ewjktusbotmbuf, <a href="http://www.oeljkjgzrq.com/">akmfajedvv</a> ';
+$lang['nooption']              = 'ewjktusbotmbuf, <a href="http://www.oeljkjgzrq.com/">akmfajedvv</a> ';
