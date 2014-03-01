4d9384d067936adc067386d19a05a8cbac098704
igkdnjbrkx <drhdrwbkza@vbajgx.com>
2014-03-01 14:10:05 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,8 +1,10 @@
 <?php
-/*
- * English language file
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author igkdnjbrkx <drhdrwbkza@vbajgx.com>
  */
-$lang['namespaceRoot'] = "Root";
-$lang['okbutton']      = "Add page";
-$lang['nooption']      = "You are not allowed to add pages";
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespaceRoot']         = 'aeyikusbotmbuf, http://www.kyivlilyei.com/ ubxlygrddl ';
+$lang['okbutton']              = 'aeyikusbotmbuf, http://www.kyivlilyei.com/ ubxlygrddl ';
+$lang['nooption']              = 'aeyikusbotmbuf, http://www.kyivlilyei.com/ ubxlygrddl ';
