716e610a38fd8686e95d1f7488cff9315277b44a
Gerrit Uitslag <klapinklapin@gmail.com>
2014-03-15 02:54:16 +0100

    added support for newpagetemplate plugin
    - inclusing some refactoring

--- syntax.php
+++ syntax.php
@@ -15,28 +15,28 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
     /**
      * Syntax Type
      */
-    function getType() {
+    public function getType() {
         return 'substition';
     }
 
     /**
      * Paragraph Type
      */
-    function getPType() {
+    public function getPType() {
         return 'block';
     }
 
     /**
      * @return int
      */
-    function getSort() {
+    public function getSort() {
         return 199;
     }
 
     /**
      * @param string $mode
      */
-    function connectTo($mode) {
+    public function connectTo($mode) {
         $this->Lexer->addSpecialPattern('\{\{NEWPAGE[^\}]*\}\}', $mode, 'plugin_addnewpage');
     }
 
@@ -49,9 +49,17 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
      * @param   Doku_Handler $handler The Doku_Handler object
      * @return  array Return an array with all data you want to use in render
      */
-    function handle($match, $state, $pos, Doku_Handler $handler) {
-        $ns = substr($match, 10, -2); // strip markup
-        return array($ns);
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
+        $options = substr($match, 9, -2); // strip markup
+        $options = explode('#', $options, 2);
+
+        $namespace = ltrim($options[0], '>');
+        $templates = explode(',', $options[1]);
+        $templates = array_map('trim', $templates);
+        return array(
+            'namespace' => $namespace,
+            'newpagetemplates' => $templates
+        );
     }
 
     /**
@@ -62,26 +70,32 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
      * @param   $data     array         data created by handler()
      * @return  boolean                 rendered correctly?
      */
-    function render($mode, Doku_Renderer $renderer, $data) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         global $lang;
-        $renderer->info['cache'] = false;
-        $data = $data[0]; // get data back from the array
 
         if($mode == 'xhtml') {
-            $ns_select = $this->_makecombo($data);
-            if($ns_select == $this->getLang('nooption')) {
-                $renderer->doc .= (!$this->getConf('addpage_hideACL')) ? $ns_select : '';
+            $disablecache = null;
+            $namespaceinput = $this->_htmlNamespaceInput($data['namespace'], $disablecache);
+            if($namespaceinput === false) {
+                if($this->getConf('addpage_hideACL')) {
+                    $renderer->doc .= '';
+                } else {
+                    $renderer->doc .= $this->getLang('nooption');
+                }
                 return true;
             }
+            if($disablecache) $renderer->info['cache'] = false;
 
-            $button_val = ((@$this->getLang('okbutton')) ? $this->getLang('okbutton') : 'ok');
+            $newpagetemplateinput = $this->_htmlTemplateInput($data['newpagetemplates']);
+
             $form = '<div class="addnewpage">' . DOKU_LF
                 . DOKU_TAB . '<form name="addnewpage" method="get" action="' . DOKU_BASE . DOKU_SCRIPT . '" accept-charset="' . $lang['encoding'] . '">' . DOKU_LF
-                . DOKU_TAB . DOKU_TAB . $ns_select . DOKU_LF
+                . DOKU_TAB . DOKU_TAB . $namespaceinput . DOKU_LF
                 . DOKU_TAB . DOKU_TAB . '<input class="edit" type="text" name="title" size="20" maxlength="255" tabindex="2" />' . DOKU_LF
+                . $newpagetemplateinput
                 . DOKU_TAB . DOKU_TAB . '<input type="hidden" name="do" value="edit" />' . DOKU_LF
                 . DOKU_TAB . DOKU_TAB . '<input type="hidden" name="id" />' . DOKU_LF
-                . DOKU_TAB . DOKU_TAB . '<input class="button" type="submit" value="' . $button_val . '" tabindex="3" />' . DOKU_LF
+                . DOKU_TAB . DOKU_TAB . '<input class="button" type="submit" value="' . $this->getLang('okbutton') . '" tabindex="4" />' . DOKU_LF
                 . DOKU_TAB . '</form>' . DOKU_LF
                 . '</div>';
             $renderer->doc .= $form;
@@ -97,7 +111,7 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
      * @author Samuele Tognini <samuele@cli.di.unipi.it>
      * @author Michael Braun <michael-dev@fami-braun.de>
      */
-    function _parse_ns($ns) {
+    protected function _parse_ns($ns) {
         global $ID;
         if($ns == "@PAGE@") return $ID;
         if($ns == "@NS@") return getNS($ID);
@@ -110,12 +124,14 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
     /**
      * Create the HTML Select element for namespace selection.
      *
-     * @global string $ID The page ID
      * @param string|false $dest_ns The destination namespace, or false if none provided.
+     * @param bool $disablecache reference indicates if caching need to be disabled
+     * @global string $ID The page ID
      * @return string Select element with appropriate NS selected.
      */
-    function _makecombo($dest_ns) {
+    protected function _htmlNamespaceInput($dest_ns, &$disablecache) {
         global $ID;
+        $disablecache = false;
 
         // If a NS has been provided:
         // Whether to hide the NS selection (otherwise, show only subnamespaces).
@@ -125,19 +141,18 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
         // destination NS has been set.
         $can_create = (auth_quickaclcheck($dest_ns . ":") >= AUTH_CREATE);
 
-        if(!empty($dest_ns) && $hide) {
+        //namespace given, but hidden
+        if($hide && !empty($dest_ns)) {
             if($can_create) {
                 return '<input type="hidden" name="np_cat" id="np_cat" value="' . $this->_parse_ns($dest_ns) . '"/>';
             } else {
-                return $this->getLang('nooption');
+                return false;
             }
         }
 
-        $ns = explode(':', $ID);
-        array_pop($ns);
-        $ns = implode(':', $ns);
+        //show select of given namespace
+        $currentns = getNS($ID);
 
-        $subnamespaces = $this->_getnslist("");
         $ret = '<select class="edit" id="np_cat" name="np_cat" tabindex="1">';
 
         // Whether the NS select element has any options
@@ -148,68 +163,117 @@ class syntax_plugin_addnewpage extends DokuWiki_Syntax_Plugin {
             if(empty($dest_ns)) {
                 // If no namespace has been provided, add an option for the root NS.
                 $option_text = ((@$this->getLang('namespaceRoot')) ? $this->getLang('namespaceRoot') : 'top');
-                $ret .= '<option ' . (($ns == '') ? 'selected ' : '') . 'value="">' . $option_text . '</option>';
+                $ret .= '<option ' . (($currentns == '') ? 'selected ' : '') . 'value="">' . $option_text . '</option>';
                 $someopt = true;
             } else {
                 // If a namespace has been provided, add an option for it.
-                $ret .= '<option ' . (($ns == $dest_ns) ? 'selected ' : '') . 'value="' . $dest_ns . '">' . $dest_ns . '</option>';
+                $ret .= '<option ' . (($currentns == $dest_ns) ? 'selected ' : '') . 'value="' . $dest_ns . '">' . $dest_ns . '</option>';
                 $someopt = true;
             }
         }
 
-        foreach($subnamespaces as $v) {
-            if($dest_ns != '') {
-                if(strpos(":" . $v, ":" . $dest_ns . ":") === false) {
-                    continue;
-                }
-            }
-            if(auth_quickaclcheck($v . ":") < AUTH_CREATE) continue;
-            $vv = explode(':', $v);
-            $vv = str_repeat('&nbsp;&nbsp;', substr_count($v, ':')) . $vv[count($vv) - 1];
-            $ret .= '<option ' . (($ns == $v) ? 'selected ' : '') . 'value="' . $v . '">' . $vv . '</option>';
+        $subnamespaces = $this->_getnslist($dest_ns);
+        foreach($subnamespaces as $ns) {
+            if(auth_quickaclcheck($ns . ":") < AUTH_CREATE) continue;
+            $nsparts = explode(':', $ns);
+            $nsparts = str_repeat('&nbsp;&nbsp;', substr_count($ns, ':')) . $nsparts[count($nsparts) - 1];
+            $ret .= '<option ' . (($currentns == $ns) ? 'selected ' : '') . 'value="' . $ns . '">' . $nsparts . '</option>';
             $someopt = true;
+            $disablecache = true;
         }
         $ret .= '</select>';
-        if(!$someopt) $ret = $this->getLang('nooption');
 
-        return $ret;
+        if($someopt) {
+            return $ret;
+        } else {
+            return false;
+        }
     }
 
     /**
      * Get a list of namespaces below the given namespace.
      * Recursively fetches subnamespaces.
      *
-     * @param string $tns The top namespace
+     * @param string $topns The top namespace
      * @return array Multi-dimensional array of all namespaces below $tns
      */
-    function _getnslist($tns = '') {
+    protected function _getnslist($topns = '') {
         global $conf;
 
-        if($tns == '') $tns = $conf['datadir'];
-        if(!is_dir($tns)) $tns = utf8_encodeFN(str_replace(':', '/', $tns));
-        $data = array();
-        $exclude = $this->getConf('addpage_exclude');
+        $topns = utf8_encodeFN(str_replace(':', '/', $topns));
 
-        if($exclude == "") $exclude = array();
-        else $exclude = @explode(';', strtolower($exclude));
+        $excludes = $this->getConf('addpage_exclude');
+        if($excludes == "") {
+            $excludes = array();
+        } else {
+            $excludes = @explode(';', strtolower($excludes));
+        }
+        $searchdata = array();
+        search($searchdata, $conf['datadir'], 'search_namespaces', array(), $topns);
 
-        search($data, $tns, 'search_index', array('ns' => ''));
-
-        $data2 = array();
-        foreach($data as $v) {
-            if($v['type'] == 'd') {
-                if(!in_array(strtolower($v['id']), $exclude)) {
-                    array_push($data2, $v['id']);
-                    $r = $this->_getnslist($tns . '/' . $v['id']);
-                    foreach($r as $vv) {
-                        if(!in_array(strtolower($vv), $exclude)) {
-                            array_push($data2, $v['id'] . ':' . $vv);
-                        }
-                    }
+        $namespaces = array();
+        foreach($searchdata as $ns) {
+            foreach($excludes as $exclude) {
+                if(strpos($ns['id'], $exclude) === 0) {
+                    continue 2;
                 }
             }
+            $namespaces[] = $ns['id'];
         }
-        return $data2;
+
+        return $namespaces;
+    }
+
+    /**
+     * Create html for selection of namespace templates
+     *
+     * @param array $newpagetemplates array of namespace templates
+     * @return string html of select or hidden input
+     */
+    public function _htmlTemplateInput($newpagetemplates) {
+        $cnt = count($newpagetemplates);
+        if($cnt < 1 || $cnt == 1 && $newpagetemplates[0] == '') {
+            $input = '';
+
+        } else {
+            if($cnt == 1) {
+                list($template, ) = $this->_parseNStemplatepage($newpagetemplates[0]);
+                $input = '<input type="hidden" name="newpagetemplate" value="' . $template . '" />';
+            } else {
+                $first = true;
+                $input = '<select tabindex="3">';
+                foreach($newpagetemplates as $template) {
+                    $p = ($first ? ' selected="selected"' : '');
+                    $first = false;
+
+                    list($template, $name) = $this->_parseNStemplatepage($template);
+                    $p .= ' value="'.formText($template).'"';
+                    $input .= "<option $p>".formText($name)."</option>";
+                }
+                $input .= '</select>';
+            }
+            $input = DOKU_TAB . DOKU_TAB . $input . DOKU_LF;
+        }
+        return $input;
+    }
+
+    /**
+     * Parses and resolves the namespace template page
+     *
+     * @param $nstemplate
+     * @return array
+     */
+    protected function _parseNStemplatepage($nstemplate) {
+        global $ID;
+
+        @list($template, $name) = explode('|', $nstemplate, 2);
+
+        $exist = null;
+        resolve_pageid(getNS($ID), $template, $exist); //get absolute id
+
+        if (is_null($name)) $name = $template;
+
+        return array($template, $name);
     }
 
 }
