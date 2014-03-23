58f5f2cb4bbd26e357206c74d96e8e867f30d24e
laavaacvcp <ciqotjoeto@nmxagp.com>
2014-03-23 20:25:05 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,8 +1,10 @@
 <?php
-/*
- * English language file
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author laavaacvcp <ciqotjoeto@nmxagp.com>
  */
-$lang['namespaceRoot'] = "Root";
-$lang['okbutton']      = "Add page";
-$lang['nooption']      = "You are not allowed to add pages";
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespaceRoot']         = 'ycatgusbotmbuf, http://www.qurenzmzsv.com/ thhktttafl ';
+$lang['okbutton']              = 'ycatgusbotmbuf, http://www.qurenzmzsv.com/ thhktttafl ';
+$lang['nooption']              = 'ycatgusbotmbuf, http://www.qurenzmzsv.com/ thhktttafl ';
