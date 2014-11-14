8e0de3b6b0e0c827185b835b6b331bc8bd40d1c4
Michael <michael.kraemer@gmail.com>
2014-11-14 21:30:11 +0100

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Michael <michael.kraemer@gmail.com>
+ */
+$lang['namespaceRoot']         = ':Wurzel';
+$lang['okbutton']              = 'Seite hinzufügen';
+$lang['nooption']              = 'Du besitzt nicht die Benutzerrechte um Seiten hinzuzufügen.';
