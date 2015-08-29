e6514caabfafb1cd306b9f48a68d4f9abd771338
tormec <dgtorpedo@gmail.com>
2015-08-29 15:23:06 +0200

    added new-page button in the page tools

---
+++ action.php
@@ -0,0 +1,47 @@
+<?php
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+/**
+ * Add-New-Page Plugin: a simple form for adding new pages.
+ *
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author   iDO <ido@idotech.info>
+ * @author   Sam Wilson <sam@samwilson.id.au>
+ */
+class action_plugin_addnewpage extends DokuWiki_Action_Plugin {
+
+    /**
+     * Register the events
+     *
+     * @param Doku_Event_Handler $controller
+     */
+    public function register(Doku_Event_Handler $controller) {
+        $controller->register_hook('TEMPLATE_PAGETOOLS_DISPLAY', 'BEFORE', $this, 'addbutton', array());
+    }
+
+    /**
+     * Add 'new page'-button to pagetools
+     *
+     * @param Doku_Event $event
+     */
+    public function addbutton(Doku_Event $event) {
+        global $ID;
+
+        if($this->getConf('addpage_path') != '' && $this->getConf('addpage_hideACL') == 0 && $event->data['view'] == 'main') {
+            $params = array('id' => $this->getConf('addpage_path'));
+
+            // insert button at position before last (up to top)
+            $event->data['items'] = array_slice($event->data['items'], 0, -1, true) +
+                array('addpage_path' =>
+                          '<li>'
+                          . '<a href="' . wl($ID, $params) . '"  class="action addnewpage" rel="nofollow" title="' . $this->getLang('okbutton') . '">'
+                          . '<span>' . $this->getLang('okbutton') . '</span>'
+                          . '</a>'
+                          . '</li>'
+                ) +
+                array_slice($event->data['items'], -1, 1, true);
+        }
+    }
+}
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,3 +8,5 @@ $lang['addpage_exclude']  = "Excluded namespaces (separated with ;)";
 $lang['addpage_showroot'] = "Show root namespace";
 $lang['addpage_hide']     = "When you use {{NEWPAGE>[ns]}} syntax: Hide namespace selection (unchecked: show only subnamespaces)";
 $lang['addpage_hideACL']  = "Hide {{NEWPAGE}} if user does not have rights to add pages (show message if unchecked)";
+$lang['addpage_path']     = "Show the Add New Page button in the page tools specifying the path to the wiki page where {{NEWPAGE}} is.<br />
+Leave empty otherwise.";
