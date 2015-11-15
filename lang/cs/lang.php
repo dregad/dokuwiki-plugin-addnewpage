5d2f27aacf02ee4bb6857dcea268f2ae9668a3b6
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2015-11-15 12:45:31 +0100

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ */
+$lang['namespaceRoot']         = 'Kořen';
+$lang['okbutton']              = 'Přidat stránku';
+$lang['nooption']              = 'Nejste oprávněni přidávat stránky';
