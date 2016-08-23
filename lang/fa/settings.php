a962842168d8516d476746444c776d122d728900
Sam01 <m.sajad079@gmail.com>
2016-08-24 00:10:34 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['addpage_exclude']       = 'حذف فضاهای نام (جدا کردن از هم با ;)';
+$lang['addpage_showroot']      = 'نمایش فضای‌نام ریشه';
+$lang['addpage_hide']          = 'زمانی که استفاده می‌کنید از {{NEWPAGE>[ns]}} نحو: مخفی سازی فضای‌نام انتخابی
+(تیک: نمایش تمام فضاهای زیر نام)';
+$lang['addpage_hideACL']       = 'مخفی کردن {{NEWPAGE}} اگر کاربران';
