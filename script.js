8640cd557ebcb2fa32c001e25096a8243e59707a
Andreas Gohr <gohr@cosmocode.de>
2017-05-30 09:13:35 +0200

    fix script to work on multiple forms again

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-jQuery(document).ready(function () {
-    var $form = jQuery(".addnewpage form");
-    if (!$form.length) return;
+jQuery(function () {
+    jQuery(".addnewpage form").each(function () {
+        var $form = jQuery(this);
+        var $ns = $form.find("[name='np_cat']");
+        var $title = $form.find("input[name='title']");
+        var $id = $form.find("input[name='id']");
+        var $submit = $form.find(':submit');
 
-    var $ns = $form.find("[name='np_cat']");
-    var $title = $form.find("input[name='title']");
-    var $id = $form.find("input[name='id']");
-    var $submit = $form.find(':submit');
+        // disable submit unless something is in input or input is disabled
+        if ($title.attr('type') === 'text') {
+            $submit.attr('disabled', 'disabled');
+            $title.keyup(function () {
+                if ($title.val().length > 0) {
+                    $submit.removeAttr('disabled');
+                } else {
+                    $submit.attr('disabled', 'disabled');
+                }
+            });
+        }
 
-    // disable submit unless something is in input or input is disabled
-    if ($title.attr('type') === 'text') {
-        $submit.attr('disabled', 'disabled');
-        $title.keyup(function () {
-            if ($title.val().length > 0) {
-                $submit.removeAttr('disabled');
-            } else {
-                $submit.attr('disabled', 'disabled');
-            }
+        // Change the form's page-ID field on submit
+        $form.submit(function () {
+            // Build the new page ID and save in hidden form field
+            var id = $ns.val().replace('@INPUT@', $title.val());
+            $id.val(id);
+
+            // Clean up the form vars, just to make the resultant URL a bit nicer
+            $ns.prop("disabled", true);
+            $title.prop("disabled", true);
+
+            return true;
         });
-    }
 
-    // Change the form's page-ID field on submit
-    $form.submit(function () {
-        // Build the new page ID and save in hidden form field
-        var id = $ns.val().replace('@INPUT@', $title.val());
-        $id.val(id);
-
-        // Clean up the form vars, just to make the resultant URL a bit nicer
-        $ns.prop("disabled", true);
-        $title.prop("disabled", true);
-
-        return true;
     });
-
 });
