aaf74414c4cc8cc7738f959423956491f0c5acb0
Joerg <scooter22@gmx.de>
2017-10-27 09:30:05 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,9 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Joerg <scooter22@gmx.de>
  */
 $lang['addpage_exclude']       = 'Namespaces ausschließen (getrennt mit ; )';
 $lang['addpage_showroot']      = 'Wurzel-Namespace anzeigen';
-$lang['addpage_hide']          = '{{NEWPAGE>[ns]}} Syntax: Ausgewählt, diese Namespace-Selektion verbergen. Nicht ausgewählt, nur diese Namespace-Selektion anzeigen.';
-$lang['addpage_hideACL']       = 'Wenn ein Benutzer keine Berechtigung hat: Ausgewählt, Anzeige wird verborgen. Nicht ausgewählt, Es wird eine Fehlermeldung ausgegeben.';
+$lang['addpage_hide']          = '{{NEWPAGE>[ns]}} Syntax: Ausgewählt, diese Namespace-Auswahl verbergen. Nicht ausgewählt, nur diese Namespace-Auswahl anzeigen.';
+$lang['addpage_hideACL']       = 'Verberge {{NEWPAGE}} wenn ein Benutzer keine Berechtigung hat Seiten hinzuzufügen
+Ausgewählt: Anzeige wird verborgen
+Nicht ausgewählt: es wird eine Fehlermeldung ausgegeben';
