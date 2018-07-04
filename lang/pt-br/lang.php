dfce2287c6c7ce2de51f2c717c7a2b051293bbee
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-04 22:05:05 +0200

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Samory Pereira Santos <samory.santos@gmail.com>
  */
 $lang['namespaceRoot']         = 'Raiz';
