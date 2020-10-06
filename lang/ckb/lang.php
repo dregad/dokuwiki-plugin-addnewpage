663ceeb14de4edde792c1a68e6c294aeaede63d7
qezwan <qezwan@gmail.com>
2020-10-06 12:10:36 +0200

    translation update

---
+++ lang/ckb/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['namespaceRoot']         = 'ڕەگ';
+$lang['okbutton']              = 'زیادکردنی لاپەڕە';
+$lang['nooption']              = 'تۆ ڕێگەت پێنەدراوە لاپەڕەزیاد بکەیت';
