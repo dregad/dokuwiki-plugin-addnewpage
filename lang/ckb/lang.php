5f7859bc3c7100129dfc7429493ce0c430f9cfff
Wrya <wryaali33@gmail.com>
2020-10-08 10:00:35 +0200

    translation update

---
+++ lang/ckb/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wrya <wryaali33@gmail.com>
+ */
+$lang['namespaceRoot']         = 'ڕەگ';
+$lang['okbutton']              = 'پەرە زیاد بکە ';
+$lang['nooption']              = 'ئیوە ڕێگە پێدراو نین بۆ زیادکردنی پەڕەکان';
