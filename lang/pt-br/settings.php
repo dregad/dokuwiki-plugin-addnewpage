3d807c1f4d5207961731f0ded3212c900cfc627e
Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
2021-01-08 18:55:06 +0100

    translation update

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -2,8 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
  * @author Samory Pereira Santos <samory.santos@gmail.com>
  */
 $lang['addpage_exclude']       = 'Excluir namespaces (separar com ;)';
 $lang['addpage_showroot']      = 'Mostrar namespace raiz';
+$lang['addpage_hide']          = 'Ao usar a sintaxe {{NEWPAGE>[ns]}}: Ocultar seleção de espaço de nomes (desmarcado: mostrar apenas subespaços de nomes)';
+$lang['addpage_hideACL']       = 'Ocultar {{NEWPAGE}} se o usuário não tiver permissão para adicionar páginas (mostrar mensagem se desmarcado)';
