c1d1cbc321255f52afd0d49d419474f34919ea93
Fekete Ádám Zsolt <fadam@egbcsoport.hu>
2023-11-14 02:55:22 +0100

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Fekete Ádám Zsolt <fadam@egbcsoport.hu>
+ */
+$lang['namespaceRoot']         = 'Kiindulónévtér';
+$lang['okbutton']              = 'Oldal hozzáadása';
+$lang['nooption']              = 'Nincs jogosultságod oldal létrehozásához';
