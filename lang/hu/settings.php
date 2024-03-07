96203ee2943024d463cb8e62d431d2726c4017ad
DokuWiki Translation Tool <admin@dokuwiki.org>
2024-03-07 19:38:17 +0100

    translation update (hu) (#99)
    Co-authored-by: Fekete Ádám Zsolt <fadam@egbcsoport.hu>

---
+++ lang/hu/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Fekete Ádám Zsolt <fadam@egbcsoport.hu>
+ */
+$lang['addpage_exclude']       = 'Elrejtett névterek (pontosvesszővel elválasztva)';
+$lang['addpage_showroot']      = 'Kiindulónévtér mutatása';
+$lang['addpage_hide']          = 'A {{NEWPAGE>[ns]}} szintaxis használatakor: Névtérválasztó lista elrejtése (ha nincs bejelölve: mutatja az alnévtereket)';
+$lang['addpage_hideACL']       = 'A {{NEWPAGE}} elrejtése ha a felhasználónak nincs jogosultsága oldal létrehozásához (ha nincs bejelölve, egy üzenet fog megjelenni)';
