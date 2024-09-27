c9e5e1b4424e63c691448bdb32139e67d5c3296e
Aleksandr Selivanov <alexgearbox@yandex.ru>
2024-09-27 10:45:42 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -2,9 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['namespaceRoot']         = 'Корневой уровень';
+$lang['name']                  = 'Имя страницы';
 $lang['okbutton']              = 'Добавить страницу';
 $lang['nooption']              = 'Вы не можете добавлять страницы';
