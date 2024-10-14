744b3441b25e492c6efe888a05b2e80b0c72cb79
MaWi <drmaxxis@gmail.com>
2024-10-14 15:35:16 +0200

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author MaWi <drmaxxis@gmail.com>
  * @author Michael <michael.kraemer@gmail.com>
  */
 $lang['namespaceRoot']         = ':Wurzel';
+$lang['name']                  = 'Seiten Name';
 $lang['okbutton']              = 'Seite hinzufügen';
 $lang['nooption']              = 'Du besitzt nicht die Benutzerrechte um Seiten hinzuzufügen.';
