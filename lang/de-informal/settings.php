57f606807bb96ca9e5baaa5bf13c322a728fa869
MaWi <drmaxxis@gmail.com>
2024-10-14 15:35:27 +0200

    translation update

--- lang/de-informal/settings.php
+++ lang/de-informal/settings.php
@@ -2,10 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author MaWi <drmaxxis@gmail.com>
  * @author Michael <michael.kraemer@gmail.com>
  */
 $lang['addpage_exclude']       = 'Namespaces ausschließen (getrennt mit ; )';
 $lang['addpage_showroot']      = 'Wurzel-Namespace anzeigen';
-$lang['addpage_hide']          = '{{NEWPAGE>[ns]}} Syntax: Ausgewählt, diese Namespace-Selektion verbergen. Nicht ausgewählt, nur diese Namespace-Selektion anzeigen.';
-$lang['addpage_hideACL']       = 'Wenn ein Benutzer keine Berechtigung hat: Ausgewählt, Anzeige wird verborgen. Nicht ausgewählt, Es wird eine Fehlermeldung ausgegeben.';
+$lang['addpage_hide']          = '{{NEWPAGE>[ns]}} Syntax: Ausgewählt, diese Namespace-Auswahl verbergen. Nicht ausgewählt, nur diese Namespace-Auswahl anzeigen.';
+$lang['addpage_hideACL']       = 'Verberge {{NEWPAGE}} wenn ein Benutzer keine Berechtigung hat Seiten hinzuzufügen
+Ausgewählt: Anzeige wird verborgen
+Nicht ausgewählt: es wird eine Fehlermeldung ausgegeben';
