fd137d7edec56b0d290eb628fb67b76cca2a5feb
小命Leaflet <2532846822@qq.com>
2024-10-31 16:00:46 +0100

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -1,8 +1,13 @@
 <?php
-/*
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * Chinese(Simplified) language file
+ *
+ * @author 小命Leaflet <2532846822@qq.com>
  */
-$lang['namespaceRoot']="根部命名空间";
-$lang['okbutton']     ="增加页面";
-$lang['nooption']     ="抱歉，您没有权限增加页面";
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['namespaceRoot']         = '根部命名空间';
+$lang['name']                  = '页面名称';
+$lang['okbutton']              = '增加页面';
+$lang['nooption']              = '抱歉，您没有权限增加页面';
