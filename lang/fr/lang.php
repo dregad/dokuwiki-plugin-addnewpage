74c22dcd1b9900601aa5c3a84c1c41cce0bc14b4
Damien Regad <dregad@mantisbt.org>
2024-11-13 12:10:56 +0100

    Translation update (fr)
    - Use standard translation 'Catégorie' for 'namespace'
    - Replace HTML entities by UTF-8 accented characters
    - Minor fixes

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -5,5 +5,5 @@
  */
 $lang['namespaceRoot'] = "Racine";
 $lang['name']          = "Nom de la page";
-$lang['okbutton']      = "Cr&eacute;er";
+$lang['okbutton']      = "Créer";
 $lang['nooption']      = "Vous n'avez pas les droits pour ajouter une page";
