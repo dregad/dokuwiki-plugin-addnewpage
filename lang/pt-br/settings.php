e5ab1083715a25653f09bf0d0ce723a1fb82cec7
DokuWiki Translation Tool <admin@dokuwiki.org>
2024-12-23 23:52:54 +0100

    Translation update (pt-br) (#111)
    Co-authored-by: Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -3,10 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
  * @author Alexandre Belchior <alexbelchior@gmail.com>
  * @author Samory Pereira Santos <samory.santos@gmail.com>
  */
 $lang['addpage_exclude']       = 'Excluir namespaces (separar com ;)';
 $lang['addpage_showroot']      = 'Mostrar namespace raiz';
-$lang['addpage_hide']          = 'Quando você usa a sintax {{NEWPAGE>[ns]}} : Oculta a seleção do namespace (desmarcado: mostra apenas os subnamespaces)';
+$lang['addpage_hide']          = 'Quando você usa a sintaxe {{NEWPAGE>[ns]}} : Oculta a seleção do namespace (desmarcado: mostra apenas os subnamespaces)';
 $lang['addpage_hideACL']       = 'Ocultar {{NEWPAGE}} se o usuário não tiver direitos para adicionar páginas (mostre a mensagem se desmarcada)';
+$lang['addpage_autopage']      = 'Não mostre a caixa de entrada, o namespace pré-configurado é tratado como um ID de página inteira. (faz sentido com <i>placeholders</i> de data)';
