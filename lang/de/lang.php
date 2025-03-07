bd4513feecdef79f35b3c5d7d75f185ae8cedbef
DokuWiki Translation Tool <admin@dokuwiki.org>
2025-03-07 16:46:05 +0100

    Translation update (de) (#114)
    Co-authored-by: analogroboter <ropely@gmx.net>

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,9 +3,11 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author analogroboter <ropely@gmx.net>
  * @author Joerg <scooter22@gmx.de>
  * @author Michael <michael@krmr.org>
  */
 $lang['namespaceRoot']         = 'Wurzel';
+$lang['name']                  = 'Seitenname';
 $lang['okbutton']              = 'Seite hinzufügen';
-$lang['nooption']              = 'Du besitzt nicht die Benutzerrechte um Seiten hinzuzufügen.';
+$lang['nooption']              = 'Du besitzt nicht die Benutzerrechte, um Seiten hinzuzufügen.';
