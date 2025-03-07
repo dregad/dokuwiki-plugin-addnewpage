f0b0c37676de6f63e1b4bb2f5acd934cd55ba37b
analogroboter <ropely@gmx.net>
2025-03-07 15:45:05 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,11 +3,13 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author analogroboter <ropely@gmx.net>
  * @author Joerg <scooter22@gmx.de>
  */
-$lang['addpage_exclude']       = 'Namespaces ausschließen (getrennt mit ; )';
-$lang['addpage_showroot']      = 'Wurzel-Namespace anzeigen';
+$lang['addpage_exclude']       = 'Namensräume ausschließen (getrennt mit ; )';
+$lang['addpage_showroot']      = 'Wurzel-Namensraum anzeigen';
 $lang['addpage_hide']          = '{{NEWPAGE>[ns]}} Syntax: Ausgewählt, diese Namespace-Auswahl verbergen. Nicht ausgewählt, nur diese Namespace-Auswahl anzeigen.';
 $lang['addpage_hideACL']       = 'Verberge {{NEWPAGE}} wenn ein Benutzer keine Berechtigung hat Seiten hinzuzufügen
 Ausgewählt: Anzeige wird verborgen
 Nicht ausgewählt: es wird eine Fehlermeldung ausgegeben';
+$lang['addpage_autopage']      = 'Zeige keine Eingabemaske, der vorkonfigurierte Namensraum wird als Gesamtseiten ID behandelt (sinnvoll mit Platzhaltern für Datumsangaben)';
