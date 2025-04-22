62a14f35b49bd40ec7cc65a6eef0e25bf4bb8e8a
DokuWiki Translation Tool <admin@dokuwiki.org>
2025-04-22 09:27:39 +0200

    translation update (cs) (#115)
    Co-authored-by: Martin Růžička <martinr@post.cz>

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Martin Růžička <martinr@post.cz>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  */
 $lang['namespaceRoot']         = 'Kořen';
+$lang['name']                  = 'Název stránky';
 $lang['okbutton']              = 'Přidat stránku';
 $lang['nooption']              = 'Nejste oprávněni přidávat stránky';
