78643afa7dae9bce14c90836cea62e463e6190d1
CL <chris.loewe@vodafone.de>
2025-09-07 09:50:40 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author CL <chris.loewe@vodafone.de>
  * @author analogroboter <ropely@gmx.net>
  * @author Joerg <scooter22@gmx.de>
  */
@@ -13,3 +14,4 @@ $lang['addpage_hideACL']       = 'Verberge {{NEWPAGE}} wenn ein Benutzer keine B
 Ausgewählt: Anzeige wird verborgen
 Nicht ausgewählt: es wird eine Fehlermeldung ausgegeben';
 $lang['addpage_autopage']      = 'Zeige keine Eingabemaske, der vorkonfigurierte Namensraum wird als Gesamtseiten ID behandelt (sinnvoll mit Platzhaltern für Datumsangaben)';
+$lang['addpage_createns']      = 'Erlaubt die Erstellung von Trennezichen. Wenn diese Option nicht aktiviert ist, werden Doppelpunkte (<code>:</code>) im Seitennamen durch Unterstriche (<code>_</code>) ersetzt.';
