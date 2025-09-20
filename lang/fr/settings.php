7c39161104bce728634a3b64c30d13aa8cb71ebe
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2025-09-20 17:30:05 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,11 +1,16 @@
 <?php
-/*USE : UTF8*/
 
-/*
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * USE : UTF8
  * French language file
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
-$lang['addpage_exclude']  = "Catégories à exclure (séparées par un ';')";
-$lang['addpage_showroot'] = "Afficher la catégorie racine";
-$lang['addpage_hide']     = "Quand vous utlisez la syntaxe {{NEWPAGE>[ns]}} : cache la selection de catégorie (décoché: affiche uniquement les sous-catégories)";
-$lang['addpage_hideACL']  = "Si non cochée, affiche un message lorsque l'utilisateur n'a pas les droits d'ajout de page. Sinon, cache simplement {{NEWPAGE}}";
-$lang['addpage_autopage'] = "Ne pas afficher le champ de saisie; la catégorie préconfigurée est traitée comme un identifiant de page complet (utile avec des attributs de date)";
+$lang['addpage_exclude']       = 'Catégories à exclure (séparées par un \';\')';
+$lang['addpage_showroot']      = 'Afficher la catégorie racine';
+$lang['addpage_hide']          = 'Quand vous utlisez la syntaxe {{NEWPAGE>[ns]}} : cache la selection de catégorie (décoché: affiche uniquement les sous-catégories)';
+$lang['addpage_hideACL']       = 'Si non cochée, affiche un message lorsque l\'utilisateur n\'a pas les droits d\'ajout de page. Sinon, cache simplement {{NEWPAGE}}';
+$lang['addpage_autopage']      = 'Ne pas afficher le champ de saisie; la catégorie préconfigurée est traitée comme un identifiant de page complet (utile avec des attributs de date)';
+$lang['addpage_createns']      = 'Autoriser la création de sous-catégories. Si non coché, le souligné (<code>_</code>) remplacera les deux points (<code>:</code>).';
