faa0ae912a1032c7d547f09780b66e7e54cac062
DokuWiki Translation Tool <admin@dokuwiki.org>
2025-09-21 18:34:19 +0200

    translation update (fr)
    PR #123
    
    Co-authored-by: Schplurtz le Déboulonné <Schplurtz@laposte.net>

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,9 +1,13 @@
 <?php
-/*USE : UTF8*/
-/*
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * USE : UTF8
  * French language file
+ *
  */
-$lang['namespaceRoot'] = "Racine";
-$lang['name']          = "Nom de la page";
-$lang['okbutton']      = "Créer";
-$lang['nooption']      = "Vous n'avez pas les droits pour ajouter une page";
+$lang['namespaceRoot']         = 'Racine';
+$lang['name']                  = 'Nom de la page';
+$lang['okbutton']              = 'Créer';
+$lang['nooption']              = 'Vous n\'avez pas les droits pour ajouter une page';
